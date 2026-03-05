3a58a54a1bfb1f1b06cc62320d6f30de964a6b5f
root <root@YFZX-WZX.localdomain>
2026-03-05 09:53:01 +0800

    Make gym part of nn.h

--- img2nn.c
+++ img2nn.c
@@ -8,6 +8,7 @@
 #include "stb_image_write.h"
 
 #define NN_IMPLEMENTATION
+#define NN_ENABLE_GYM
 #include "nn.h"
 
 char *args_shift(int *argc, char ***argv)
@@ -17,96 +18,6 @@ char *args_shift(int *argc, char ***argv)
     (*argc) -= 1;
     (*argv) += 1;
     return result;
-}
-typedef struct {
-    float *items;
-    size_t count;
-    size_t capacity;
-} Cost_Plot;
-
-#define DA_INIT_CAP 256
-#define da_append(da, item)								\
-do {											\
-    if(((da))->count >= ((da))->capacity) {						\
-	(da)->capacity = (da)->capacity == 0 ? DA_INIT_CAP : (da)->capacity*2;		\
-	(da)->items = realloc((da)->items, (da)->capacity*sizeof(*(da)->items));	\
-	assert((da)->items != NULL && "Buy more RAM lol");				\
-    }											\
-    (da)->items[(da)->count++] = (item);						\
-} while(0)										\
-
-void nn_render_raylib(NN nn, int rx, int ry, int rw, int rh)
-{
-    Color low_color = {0xFF, 0x00, 0xFF, 0xFF};
-    Color high_color = {0x00, 0xFF, 0x00, 0xFF};
-
-    float neuron_radius = rh*0.03;
-    int layer_border_vpad = rh*0.08; // horizon pad
-    int layer_border_hpad = rh*0.06; // vertical pad
-
-    int nn_width = rw - 2*layer_border_hpad; // minus left and right pad
-    int nn_height = rh - 2*layer_border_vpad; // minus top and bottom pad
-
-    int nn_x = rx + rw/2 - nn_width/2; // rx + layer_border_hpad
-    int nn_y = ry + rh/2 - nn_height/2; // ry + layer_border_vpad
-
-    size_t arch_count = nn.count + 1;
-    float layer_hpad = nn_width / arch_count; // the average width of each layer could occupy
-
-    for(size_t l = 0; l < arch_count; ++l) {
-	float layer_vpad1 = nn_height / nn.as[l].cols; // the average height of each neural plot of each layer
-	for(size_t i = 0; i < nn.as[l].cols; ++i) {
-	    float cx1 = nn_x + l*layer_hpad + layer_hpad/2;
-	    float cy1 = nn_y + i*layer_vpad1 + layer_vpad1/2;
-	    if(l+1 < arch_count) { // draw line to next neural plot if not the last layer
-		float layer_vpad2 = nn_height / nn.as[l+1].cols; 
-		for(size_t j = 0; j < nn.as[l+1].cols; ++j) {
-		    float cx2 = nn_x + (l+1)*layer_hpad + layer_hpad/2;
-		    float cy2 = nn_y + j*layer_vpad2 + layer_vpad2/2;
-		    float value = sigmoidf(MAT_AT(nn.ws[l], i, j));
-		    high_color.a = floorf(255.f*value);
-		    float thick = rh*0.004f;
-		    Vector2 start = {cx1, cy1};
-		    Vector2 end = {cx2, cy2};
-		    DrawLineEx(start, end, thick, ColorAlphaBlend(low_color, high_color, WHITE)); // core function which do the operation
-		}
-	    }
-
-	    if(l > 0) {
-		high_color.a = floorf(255.f*sigmoidf(MAT_AT(nn.bs[l-1], 0, i)));
-		DrawCircle(cx1, cy1, neuron_radius, ColorAlphaBlend(low_color, high_color, WHITE));
-	    } else {
-		DrawCircle(cx1, cy1, neuron_radius, GRAY);
-	    }
-	}
-    }
-}
-
-void plot_cost(Cost_Plot plot, int rx, int ry, int rw, int rh)
-{
-    float min = FLT_MAX, max = FLT_MIN;
-
-    for(size_t i = 0; i < plot.count; ++i) {
-	if(max < plot.items[i]) max = plot.items[i];
-	if(min > plot.items[i]) min = plot.items[i];
-    }
-    if(min > 0) min = 0; // make sure the start position is always from zero
-
-    size_t n = plot.count;
-
-    if(n < 1000) n = 1000;
-
-    for(size_t i =0; i+1 < plot.count; ++i) {
-	float x1 = rx + (float)rw/n*i;
-	float y1 = ry + (1 - (plot.items[i] - min)/(max - min))*rh;
-	float x2 = rx + (float)rw/n*(i+1);
-	float y2 = ry + (1 - (plot.items[i+1] - min)/(max - min))*rh;
-	DrawLineEx((Vector2){x1, y1}, (Vector2){x2, y2}, rh*0.005, RED);
-    }
-
-    float y0 = ry + (1 - (0-min)/(max - min))*rh;
-    DrawLineEx((Vector2){rx + 0, y0}, (Vector2){rx + rw - 1, y0}, rh*0.005, WHITE);
-    DrawText("0", rx + 0, y0 -rh*0.04, rh*0.04, WHITE);
 }
 
 int main(int argc, char **argv)
@@ -164,7 +75,7 @@ int main(int argc, char **argv)
 	.es = &MAT_AT(t, 0, ti.cols),
     };
 
-    size_t arch[] = {2,7,5,1};
+    size_t arch[] = {2, 7, 7, 1};
 
     NN nn = nn_alloc(arch, ARRAY_LEN(arch));
     NN g = nn_alloc(arch, ARRAY_LEN(arch));
@@ -178,7 +89,7 @@ int main(int argc, char **argv)
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT,"img2mat");
     SetTargetFPS(60);
 
-    Cost_Plot plot = {0};
+    Plot plot = {0};
 
     Image preview_image = GenImageColor(img_width, img_height, BLACK);
     Texture2D preview_texture = LoadTextureFromImage(preview_image);
@@ -229,11 +140,11 @@ int main(int argc, char **argv)
 	    int rh = h*2/3;
 	    int rx = 0;
 	    int ry = h/2 - rh/2;
-	    plot_cost(plot, rx, ry, rw, rh);
+	    gym_plot(plot, rx, ry, rw, rh);
 
 	    // draw the picture of nn
 	    rx += rw;
-	    nn_render_raylib(nn, rx, ry, rw, rh);
+	    gym_render_nn(nn, rx, ry, rw, rh);
 
 
 	    // draw the img
